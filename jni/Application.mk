f17c969407164e7ebf748ed18e7936e67ed11d0b
Manuel Martin <manuel.martin@wearebase.com>
2017-01-04 09:49:43 +0000

    Add armeabi-v7a

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,8 @@
 NDK_TOOLCHAIN_VERSION := 4.9
-APP_PLATFORM := android-21
+APP_PLATFORM := android-18
 APP_STL := gnustl_static
-APP_ABI := armeabi-v7a x86 x86_64#armeabi #all
+APP_CPPFLAGS := -frtti -fexceptions -std=c++11
+APP_ABI := armeabi-v7a #x86 #x86_64 #armeabi #all
+TARGET_ARCH_ABI := armeabi-v7a
 APP_MODULE := libdlib
+APP_OPTIM := release
